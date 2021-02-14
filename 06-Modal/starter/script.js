676345330b70b67c1cea88d6d7d1807b06a04ad7
MirShayan <shayan.ojaghi.mir@gmail.com>
2021-02-14 12:31:47 +0330

    06-Modal Completed

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const openMoldalButtons = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModalButton = document.querySelector(".close-modal");
+const bodyElement = document.querySelector("body");
+
+function closeModal() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+function openModal() {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+for (let i = 0; i < openMoldalButtons.length; i++) {
+    openMoldalButtons[i].addEventListener("click", openModal);
+}
+
+bodyElement.addEventListener("click", function (e) {
+    console.log(e);
+    if (e.target === closeModalButton || e.target === overlay) closeModal();
+});
+
+bodyElement.addEventListener("keydown", function (e) {
+    console.log(e);
+    if (e.key === "Escape") closeModal();
+});
+
